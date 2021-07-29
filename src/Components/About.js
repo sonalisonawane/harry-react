03e2d345caaa570368314f1b882428ef1e3a5cab
sonali sonawane <sonawanesonali41@gmail.com>
2021-07-29 18:45:00 +0530

    about page

---
+++ src/Components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const About = () =>{
+    return(
+        <>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Aperiam, sapiente impedit. Maiores autem dolores ea! 
+            Illo quibusdam nostrum rem debitis officiis necessitatibus asperiores,
+             reiciendis a? Quod reprehenderit animi iure, illum at iste distinctio deserunt.
+       </p>
+        </>
+    )
+}
